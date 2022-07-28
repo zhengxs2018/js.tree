74cc6a7f7a7d072e83bbc2cf1304d1ab615a07c2
zhengxs <zhenxs2018@foxmail.com>
2022-07-28 11:49:37 +0800

    style: 改为指定函数导出

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-export * from './types'
+export type { ID, None, Row, Node, Exporter, Transform, Predicate } from './types'
 
-export * from './operators/map'
-export * from './operators/each'
-export * from './operators/filter'
-export * from './operators/exclude'
-export * from './operators/repairWith'
+export { map } from './operators/map'
+export { each } from './operators/each'
+export { filter } from './operators/filter'
+export { exclude } from './operators/exclude'
+export { repairWith } from './operators/repairWith'
 
-export * from './transform/toTree'
-export * from './transform/toRows'
+export { toTree } from './transform/toTree'
+export type { ToTreeOptions } from './transform/toTree'
 
-export * from './common/parse'
-export * from './common/utils'
-export * from './common/constants'
+export { toRows } from './transform/toRows'
+
+export { parse } from './common/parse'
+export type { ParseOptions, ParseResult } from './common/parse'
+
+export { exporter } from './common/utils'
+export { ROOT_ID, ID_KEY, PARENT_ID_KEY, CHILDREN_KEY } from './common/constants'
 
 /**
  * 发布版本
